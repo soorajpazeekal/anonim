48366c9c1f1af89ab5bfc3c86dc5e1c7c5dfc87f
sooraj <soorajpazeekal@india.com>
2019-05-18 12:52:32 +0530

    18/05-update settings.py

--- anonimstart.py
+++ anonimstart.py
@@ -8,13 +8,11 @@ print (f.renderText('anonim'))
 print('\n\n\n@SNIP3RS7\n\n--SNIP3RS7@COCK.LI\n\n\n \n***Make sure you have an active internet connection:\n')
 user_input = input('Press YES to start application!')
 if user_input == 'yes':
-	error_log =  'zenity --info --text="Anonim will start shortly.\n" --title="Information"'
-	webbrowser.open('http://127.0.0.1:4653')
-
-	os.system (error_log)
-	cmd = 'python manage.py runserver 0:4653'
-
+	cmd = 'alias python=python3'
 	os.system(cmd)
+	cmd = 'python manage.py runserver 0:4653'
+	os.system(cmd)
+	webbrowser.open('http://127.0.0.1:4653')
 else:
 	print('Please enter right option:')
 
